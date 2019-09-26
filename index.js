622db7c319c8cb94ba497813dde001f3abd36cdf
IshjotSingh97 <ishjotsinghahluwalia@gmail.com>
2019-09-26 23:46:15 +0530

    express route added

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const path = require("path");
 const bodyparser = require("body-parser");
 const expressHandlebars = require("express-handlebars");
 const app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+app.get("/",(req,res)=>{
+    res.send("Root");
+})
+
+app.listen("3000",()=>{
+    console.log("Server started on port 3000");
+})
