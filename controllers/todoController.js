95c40b43aec8168b6f0f7881ab976230ff4575b3
IshjotSingh97 <ishjotsinghahluwalia@gmail.com>
2019-10-02 01:35:55 +0530

    controller created

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const todo = mongoose.model('todo');
 var router = express.Router();
 router.get('/',(req,res)=>{
 	res.render('todo/addoredit.hbs',{
 		viewTitle : "Insert Todo"
 	});
+})
+router.post('/add',(req,res)=>{
+	res.send(req.body);
 })
 module.exports = router;
--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const todoController = require("./controllers/todoController");
 const express = require("express");
 const path =require("path");
 const exphbs = require("express-handlebars");
+const bodyparser = require("body-parser");
 const app = express();
+
 app.listen("3000",()=>{
     console.log("Server started on port 3000");
 })
+
 app.set('viewengine','hbs');
 app.set("views",path.join(__dirname,'/views/'));
 app.engine("hbs",exphbs({
     extname: "hbs",
-    defaultLayout: "mainlayout",
+    defaultLayout: "mainLayout",
     layoutsDir: __dirname + "/views/layouts/"
-}))
+}));
 
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
+app.use(bodyparser.json());
 
 app.use("/todo",todoController);
