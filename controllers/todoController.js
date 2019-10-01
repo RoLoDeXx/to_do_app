6c12e7f853040f693d83dc960f787e0077011d98
IshjotSingh97 <ishjotsinghahluwalia@gmail.com>
2019-10-02 03:00:13 +0530

    post todo form created db working with insertion

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const todo = mongoose.model('todo');
+const todo = mongoose.model('Todo');
 var router = express.Router();
+
 router.get('/',(req,res)=>{
 	res.render('todo/addoredit.hbs',{
 		viewTitle : "Insert Todo"
 	});
 })
+
+router.get('/list',(req,res)=>{
+	var x = todo.find({title: "homework"});
+	res.json(x);
+})
+
 router.post('/add',(req,res)=>{
-	res.send(req.body);
+	var todovar = new todo();
+	todovar.title = req.body.tasktitle;
+	todovar.description = req.body.taskdescription;
+	todovar.save((err,doc)=>{
+		if(!err)
+		{
+			res.redirect('/todo/list');
+		}
+		else{
+			console.log(err);
+		}
+	});
 })
 module.exports = router;
